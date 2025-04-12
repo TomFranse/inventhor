4629ef021900e811d1bd811500672f03f3ca175a
Tom Franse <tom@tmi.one>
2025-04-12 10:11:16 +0200

    adding inventhor styling 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import theme from './theme';
 import Header from './components/Header';
 import ChatComponent from './components/ChatComponent';
 import Card from './components/Card';
+import './App.css';
 
 // Dummy data for cards
 const cardData = [
